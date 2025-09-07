2ce9e5fd02f9b0a5386f67f5bb96722a44a15d51
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-09-07 14:44:19 +0530

    `feat: next-theme`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from 'next-themes'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,7 +27,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+         <ThemeProvider
+            attribute='class'
+            enableSystem={false}
+            defaultTheme='light'>
         {children}
+        </ThemeProvider>
       </body>
     </html>
   )
